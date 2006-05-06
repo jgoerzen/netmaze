737311a540f7c088c6556f6b52e9ebbf7194d2b0
John Goerzen <jgoerzen@complete.org>
2006-05-05 19:19:52 -0500

    Imported Debian patch 0.81+jpg0.82-12

--- xnetserv-setup.tcl
+++ xnetserv-setup.tcl
@@ -1,6 +1,6 @@
 # Put the directory to your Tix shared library here.
 # Here are some examples:
 # set tixfile /home/mydir/lib/libtix.so
-set tixfile /usr/lib/libtix.so
+set tixfile /usr/lib/libTix8.4.so
 # set tixfile /usr/users/User10/jgoerzen/public/bin/lib/libtix.so
 
